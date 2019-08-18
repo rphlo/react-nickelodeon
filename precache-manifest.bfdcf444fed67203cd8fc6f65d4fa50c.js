6df79dd93e9a4ad0212771d371a8c0d40a68c75c
Raphaël Stefanini <rphl@rphl.net>
2019-08-18 16:47:59 +0300

    Updates

---
+++ precache-manifest.bfdcf444fed67203cd8fc6f65d4fa50c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85292399f746c0357e05bb21b089c77f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "033438d07df16357dd34",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5ee5ad1c75d6a989a409",
+    "url": "/static/js/2.93b5c4f8.chunk.js"
+  },
+  {
+    "revision": "033438d07df16357dd34",
+    "url": "/static/js/main.b71af839.chunk.js"
+  },
+  {
+    "revision": "8727e59561063c38f88b",
+    "url": "/static/js/runtime~main.38d63f63.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac62292765ff779b61b34f929f7daf0f.js"
+  "/precache-manifest.bfdcf444fed67203cd8fc6f65d4fa50c.js"
 );
 
 self.addEventListener('message', (event) => {
