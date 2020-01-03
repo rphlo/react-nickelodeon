c4ccb8768d65bb61f916f38b805838c8b6bd8fdd
Raphaël Stefanini <raphael.stefanini@ecom.fi>
2020-01-03 08:50:46 +0200

    Add sentry

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
 
+Sentry.init({ dsn: 'https://b31de5237b044454b3b61080061d6abd@sentry.io/273693' });
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
